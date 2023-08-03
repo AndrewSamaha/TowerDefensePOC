5c74301dcc05cea0a1a896762ec93496c0067ad8
Andrew Samaha <andrew.samaha@gmail.com>
2023-08-03 07:54:06 -0400

    feat: click to make a tower

--- src/components/Layer/Layer.jsx
+++ src/components/Layer/Layer.jsx
@@ -3,22 +3,14 @@ import PropTypes from 'prop-types';
 import { Char } from './Char/Char';
 import { observer } from "@legendapp/state/react"
 import { charsObservable, addChar } from '../../state/chars';
-import { makeBug } from '../../generators/units';
+import { makeBug, makeTower } from '../../generators/units';
 import { Explosion } from './Explosion/Explosion';
 
 const layerPadding = 10;
 
 export const Layer = observer(({ zIndex=0, clickable, mapParams }) => {
-  //console.log('layer render', Date.now())
-  const interactive = charsObservable.interactive.idArray.use();
-  const independent = charsObservable.independent.idArray.use();
-
   const interactiveIdArray = charsObservable.interactive.idArray.get();
-  // const interactiveDict = charsObservable.interactive.dict.get();
   const independentIdArray = charsObservable.independent.idArray.get();
-  // const independentDict = charsObservable.independent.dict.get();
-
-  if (clickable) 1;
 
   const charMapParams = {
     width: mapParams.width - layerPadding * 2,
@@ -32,26 +24,31 @@ export const Layer = observer(({ zIndex=0, clickable, mapParams }) => {
   }, [interactiveIdArray.length])
   
   return (
-    <div style={{
-      position: 'absolute',
-      zIndex: zIndex,
-      backgroundColor: 'black', 
-      border: '2px',
-      borderColor: 'black',
-      boxSizing: 'border-box',
-      width: `${mapParams.width}px`,
-      height: `${mapParams.height}px`,
-      padding: `${layerPadding}px`,
-      margin: '0'
-      }}>
-        {/* <Explosion pos={{
-            x: 100,
-            y: 100,
-            dir: 0,
-            speed: 100
-          }}
-          mapParams={mapParams}
-          /> */}
+    <div
+      onMouseDown={(e) => {
+        console.log(e.nativeEvent)
+        addChar('interactive', {
+          ...makeTower(),
+          pos: {
+            x: e.nativeEvent.layerX, // e.clientX,
+            y: e.nativeEvent.layerY, // e.clientY,
+            dir: Math.PI/2,
+            speed: 0
+          }
+          });
+      }}
+      style={{
+        position: 'absolute',
+        zIndex: zIndex,
+        backgroundColor: 'black', 
+        border: '2px',
+        borderColor: 'black',
+        boxSizing: 'border-box',
+        width: `${mapParams.width}px`,
+        height: `${mapParams.height}px`,
+        padding: `${layerPadding}px`,
+        margin: '0'
+        }}>
           {
             independentIdArray.map((id) => {
               
